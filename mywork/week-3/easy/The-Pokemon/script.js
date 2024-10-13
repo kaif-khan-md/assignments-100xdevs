0ca3c7d5a70b05669a16713cd9f11e69fa92a9dc
Kaif khan Md <mdkaifkhan5309@gmail.com>
2024-10-13 11:37:59 +0530

    Updated the assignment-The-Pokemon

--- mywork/week-3/easy/The-Pokemon/script.js
+++ mywork/week-3/easy/The-Pokemon/script.js
@@ -1,35 +1,33 @@
-// Replace 'your-api-key' with your actual Pokémon TCG API key
-const apiKey = 'fd57d329-19b1-4e01-8183-5c77b056aac7';
+import { config } from "dotenv";
+config();
+const apiKey = process.env.POKEMON_TCG_API_KEY;
 
-// Listen for the form submission
 function fetchPokemon() {
-    // Get user inputs
+    
     const numCards = document.getElementById('NumofPokemon').value;
-    const category = document.getElementById('typeofPokemon').value;
+    const category = document.getElementById('category').value;
 
     if(numCards === " " && category === " ")
     {
       alert("Enter all the fields");
     }
 
-    // Call function to fetch the Pokémon cards based on user input
     fetchPokemonCards(category, numCards);
 }
 
-// Function to fetch Pokémon cards
+
 function fetchPokemonCards(category, numCards) {
-    // API URL for fetching cards by category (type)
+
     const apiUrl = `https://api.pokemontcg.io/v2/cards?q=types:${category}&pageSize=${numCards}`;
 
-    // Fetch the cards
     fetch(apiUrl, {
         headers: {
-            'X-Api-Key': apiKey // Pass your API key in the headers
+            'X-Api-Key': apiKey 
         }
     })
     .then(response => response.json())
     .then(data => {
-        // Display the fetched cards on the page
+        
         displayCards(data.data);
     })
     .catch(error => {
@@ -37,23 +35,23 @@ function fetchPokemonCards(category, numCards) {
     });
 }
 
-// Function to display the cards
+
 function displayCards(cards) {
     const resultsDiv = document.getElementById('card-results');
-    resultsDiv.innerHTML = ''; // Clear previous results
+    resultsDiv.innerHTML = '';
 
     if (cards.length === 0) {
         resultsDiv.innerHTML = '<center><p>No cards found!</p></center>';
         return;
     }
     resultsDiv.innerHTML = '<center><p>Below are the Pokemons you searched for </p></center>';
-    // Loop through the cards and display them
+    
     cards.forEach(card => {
         const cardDiv = document.createElement('div');
         cardDiv.classList.add('card');
       
         
-        // Display card image and name
+    
         cardDiv.innerHTML = `
             <center>
             <h3>${card.name}</h3>
