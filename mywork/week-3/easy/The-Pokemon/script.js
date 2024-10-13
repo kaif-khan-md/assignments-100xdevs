9e2da967cdcfd2401c2309ba92dc82bd5f7b1d9e
Kaif khan Md <mdkaifkhan5309@gmail.com>
2024-10-13 11:54:36 +0530

    Updated the assignment-The-Pokemon

---
+++ mywork/week-3/easy/The-Pokemon/script.js
@@ -0,0 +1,64 @@
+import { config } from "dotenv";
+config();
+const apiKey = process.env.POKEMON_TCG_API_KEY;
+
+function fetchPokemon() {
+    
+    const numCards = document.getElementById('NumofPokemon').value;
+    const category = document.getElementById('category').value;
+
+    if(numCards === " " && category === " ")
+    {
+      alert("Enter all the fields");
+    }
+
+    fetchPokemonCards(category, numCards);
+}
+
+
+function fetchPokemonCards(category, numCards) {
+
+    const apiUrl = `https://api.pokemontcg.io/v2/cards?q=types:${category}&pageSize=${numCards}`;
+
+    fetch(apiUrl, {
+        headers: {
+            'X-Api-Key': apiKey 
+        }
+    })
+    .then(response => response.json())
+    .then(data => {
+        
+        displayCards(data.data);
+    })
+    .catch(error => {
+        console.error('Error fetching Pokémon cards:', error);
+    });
+}
+
+
+function displayCards(cards) {
+    const resultsDiv = document.getElementById('card-results');
+    resultsDiv.innerHTML = '';
+
+    if (cards.length === 0) {
+        resultsDiv.innerHTML = '<center><p>No cards found!</p></center>';
+        return;
+    }
+    resultsDiv.innerHTML = '<center><p>Below are the Pokemons you searched for </p></center>';
+    
+    cards.forEach(card => {
+        const cardDiv = document.createElement('div');
+        cardDiv.classList.add('card');
+      
+        
+    
+        cardDiv.innerHTML = `
+            <center>
+            <h3>${card.name}</h3>
+            <img src="${card.images.small}" alt="${card.name}">
+            </center>
+        `;
+
+        resultsDiv.appendChild(cardDiv);
+    });
+}
